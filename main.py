a05c6ae03ebf9ab80eac91e0d842fa730a58a0da
Yuriy Stark <68790201+Turash-eron@users.noreply.github.com>
2022-04-02 01:44:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,8 @@ server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
-
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo(message):
-    bot.reply_to(message, message.text)
+    username = message.from_user.username
+    bot.reply_to(message, f"Hello, {username}!"
 
 
 @server.route(f"/{TOKEN}", methods=["POST"])
@@ -23,13 +19,6 @@ def get_message():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
-    return '!', 200
-
-
-@server.route('/')
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
     return '!', 200
 
 
