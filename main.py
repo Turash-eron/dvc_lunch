230bae6af58a5bbe9fabb0185e3f2aa663fc2f0a
Yuriy Stark <68790201+Turash-eron@users.noreply.github.com>
2022-04-02 01:36:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ def webhook():
     return '!', 200
 
 
-if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
