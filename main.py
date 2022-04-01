f19c4e29893bad220266e76cab1515d46a3b4fd5
Yuriy Stark <68790201+Turash-eron@users.noreply.github.com>
2022-04-02 01:34:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def echo(message):
     bot.reply_to(message, message.text)
 
 
-@server.route('/' + TOKEN, methods=['POST'])
+@server.route(f"/{BOT_TOKEN}", methods=["POST"])
 def get_message():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
