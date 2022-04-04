051a06f561514403f6e2351f6800b27a246faa91
Yuriy Stark <68790201+Turash-eron@users.noreply.github.com>
2022-04-04 10:34:18 +0300

    Update New_main.py

--- New_main.py
+++ New_main.py
@@ -1,26 +1,25 @@
-# Source:  https://habr.com/ru/post/655965/
-# Source2: https://vc.ru/newtechaudit/384017-sozdaem-telegram-bot-s-vebhukami-na-heroku
-
+#  Importing libs
 import logging
 import os
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils.executor import start_webhook
 from aiogram import Bot, types
+import asyncio
+import aioschedule
 
-
+#  Getting constants from Heroku environment 
 TOKEN = os.getenv('BOT_TOKEN')
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
-# webhook settings
+#  Webhook setting and paths 
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
 WEBHOOK_PATH = f'/webhook/{TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
-# webserver settings
+#  Webservers settings (port definition)
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = os.getenv('PORT', default=8000)
 
