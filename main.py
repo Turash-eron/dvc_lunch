ab4e426cb41a314e7df425e90887c708a144a81c
Yuriy Stark <68790201+Turash-eron@users.noreply.github.com>
2022-04-12 23:06:05 +0300

    Time is corrected

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ async def lunch_print():
     #await bot.send_message(-596089645, "Lunch time! Приятного!")
 #  Function --- Schedules a timer to notify about lunch 
 async def scheduler():
-    aioschedule.every().day.at("10:07").do(lunch_print)
+    aioschedule.every().day.at("10:00").do(lunch_print)
         
     #  Completing the precess until there are no more in queue
     while True:
